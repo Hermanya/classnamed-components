91dc07e21d7d97df5c1242859ce0e8924a8fa74f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-27 17:02:51 +0000

    Deploy Hermanya/classnamed-components to github.com/Hermanya/classnamed-components.git:gh-pages

---
+++ precache-manifest.b185fc56692dac428415916c270900df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6987ffb6488c9858e831988877538386",
+    "url": "/classnamed-components/static/media/tailwind.6987ffb6.svg"
+  },
+  {
+    "revision": "6ba2a3f5311b75ebc09627b5c4b726e8",
+    "url": "/classnamed-components/static/media/tachyons.6ba2a3f5.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/classnamed-components/static/media/react.1a36cc4d.svg"
+  },
+  {
+    "revision": "ba23603c8668f4fe65dc2c7996053224",
+    "url": "/classnamed-components/static/media/bootstrap.ba23603c.svg"
+  },
+  {
+    "revision": "5a51d482fa40d12db4e7",
+    "url": "/classnamed-components/static/js/runtime~main.5a51d482.js"
+  },
+  {
+    "revision": "33bc6e87454a6f7f24fb",
+    "url": "/classnamed-components/static/js/main.33bc6e87.chunk.js"
+  },
+  {
+    "revision": "7e3b8afda60a4f0a0274",
+    "url": "/classnamed-components/static/js/1.7e3b8afd.chunk.js"
+  },
+  {
+    "revision": "33bc6e87454a6f7f24fb",
+    "url": "/classnamed-components/static/css/main.304243c1.chunk.css"
+  },
+  {
+    "revision": "6022fc68c099179348dee46c5a0e4378",
+    "url": "/classnamed-components/index.html"
+  }
+];
